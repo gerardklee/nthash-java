f5e27dee7ec325d41c6dcf28ac8366bffa706f41
GerardProject <gerardleelab@gmail.com>
2019-07-09 10:30:14 -0700

    Time comparison between getIndex and getIndexFast

--- src/dna/Main.java
+++ src/dna/Main.java
@@ -5,14 +5,7 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-//		DNA randomStr1 = new DNA(10);
-//		System.out.println(randomStr1);
-//		DNA kmer = new DNA(2);
-//		System.out.println(kmer);
-//		System.out.println(randomStr1.getIndex(kmer));
-//		System.out.println(5/2);
-//		System.out.println(5/3);
-		
+		//warm up getIndex
 		for (int i = 0; i < 10000; i++) {
 			DNA randomStr2 = new DNA(100);
 			DNA kmerrandom = new DNA(100);
@@ -21,7 +14,7 @@ public class Main {
 		DNA generateSeq = new DNA(10000000);
 		DNA generatekmer = new DNA(10000);
 		
-		// average
+		// getIndexaverage
 		double cnt = 0;
 		for (int i = 0; i < 100; i++) {
 			long start = System.nanoTime();
@@ -29,7 +22,7 @@ public class Main {
 			long elapse = System.nanoTime() - start;
 			cnt += elapse;
 		}
-		double avg = cnt / 100 / 1e6;
+		double avg = cnt / 100 * 1e-6;
 		System.out.println(avg);
 		
 		// warm up getIndexFast
@@ -41,7 +34,7 @@ public class Main {
 		DNA generateSeq1 = new DNA(10000000);
 		DNA generatekmer1 = new DNA(10000);
 		
-		// getfast average
+		// getIndexFast average
 		double cnt1 = 0;
 		for (int i = 0; i < 100; i++) {
 			long start = System.nanoTime();
@@ -51,6 +44,7 @@ public class Main {
 		}
 		double avg1 = cnt1 / 100 / 1e6;
 		System.out.println(avg1);
+
 	}
 	
 
