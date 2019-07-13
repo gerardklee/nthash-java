ae37b5f8c5f2febe59de4dc8cef5b7b4b2c52bc5
GerardProject <gerardklee@hotmail.com>
2019-07-13 11:58:16 -0700

    time check hash vs. brute force

--- src/dna/Main.java
+++ src/dna/Main.java
@@ -1,6 +1,8 @@
 package dna;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -48,49 +50,99 @@ public class Main {
 		
 		// *** TEST FOR ROLLINGHASH BEGINS HERE ***
 		
-		DNA seq = new DNA("AGTCAATTTAGATACAATATTTTAGAGAGAGATATAACAA");
-		DNA kmer = new DNA("CAA");
-		System.out.println(seq.getIndexHash(kmer));	
-		System.out.println(seq.getIndex(kmer));
+//		DNA seq = new DNA("AGTCAATTTAGATACAATATTTTAGAGAGAGATATAACAA");
+//		DNA kmer = new DNA("CAA");
+//		System.out.println(seq.getIndexHash(kmer));	
+//		System.out.println(seq.getIndex(kmer));
+//		
+//		//warm up getIndex
+//		for (int i = 0; i < 10000; i++) {
+//			DNA randomStr2 = new DNA(100);
+//			DNA kmerrandom = new DNA(100);
+//			randomStr2.getIndex(kmerrandom);
+//		}
+//		DNA generateSeq = new DNA(10000000);
+//		DNA generatekmer = new DNA(10000);
+//		
+//		// getIndexaverage
+//		double cnt = 0;
+//		for (int i = 0; i < 100; i++) {
+//			long start = System.nanoTime();
+//			generateSeq.getIndex(generatekmer);
+//			long elapse = System.nanoTime() - start;
+//			cnt += elapse;
+//		}
+//		double avg = cnt / 100 * 1e-6;
+//		System.out.println("brute force: " + avg);
+//		
+//		//warm up getIndexHash
+//		for (int i = 0; i < 10000; i++) {
+//			DNA randomStr2 = new DNA(100);
+//			DNA kmerrandom = new DNA(100);
+//			randomStr2.getIndexHash(kmerrandom);
+//		}
+//		DNA generateSeq2 = new DNA(10000000);
+//		DNA generatekmer2 = new DNA(10000);
+//		
+//		// getIndexaverage
+//		double cnt2 = 0;
+//		for (int i = 0; i < 100; i++) {
+//			long start = System.nanoTime();
+//			generateSeq2.getIndexHash(generatekmer2);
+//			long elapse = System.nanoTime() - start;
+//			cnt2 += elapse;
+//		}
+//		double avg2 = cnt2 / 100 * 1e-6;
+//		System.out.println("hash: " + avg2);
+		
+		// *** TEST END HERE ***
+
+		List<DNA> kmers = new ArrayList<>();
+		DNA generateSeq = new DNA(10000000);
+		
+		for (int i = 0; i < 100; i++) {
+			DNA kmerrandom = new DNA(100);
+			kmers.add(kmerrandom);
+		}
+		
+		
+		Map<Long, List<Integer>> result = generateSeq.buildIndex(100);
+		
+		
+		//warm up getMap
+		for (int i = 0; i < 20000; i++) {
+			DNA kmerrandom = new DNA(100);
+			generateSeq.findIndex(result, kmerrandom);
+		}
+		
+		// getIndexMap
+		double cnt1 = 0;
+		for (int i = 0; i < 100; i++) {
+			long start = System.nanoTime();
+			generateSeq.findIndex(result, kmers.get(i));
+			long elapse = System.nanoTime() - start;
+			cnt1 += elapse;
+		}
+		double avg1 = cnt1 / 100 * 1e-6;
+		System.out.println("map find: " + avg1);
 		
 		//warm up getIndex
-		for (int i = 0; i < 10000; i++) {
-			DNA randomStr2 = new DNA(100);
+		for (int i = 0; i < 20000; i++) {
+			DNA random = new DNA(100);
 			DNA kmerrandom = new DNA(100);
-			randomStr2.getIndex(kmerrandom);
+			random.getIndex(kmerrandom);
 		}
-		DNA generateSeq = new DNA(10000000);
-		DNA generatekmer = new DNA(10000);
 		
 		// getIndexaverage
 		double cnt = 0;
 		for (int i = 0; i < 100; i++) {
 			long start = System.nanoTime();
-			generateSeq.getIndex(generatekmer);
+			generateSeq.getIndex(kmers.get(i));
 			long elapse = System.nanoTime() - start;
 			cnt += elapse;
 		}
 		double avg = cnt / 100 * 1e-6;
 		System.out.println("brute force: " + avg);
 		
-		//warm up getIndexHash
-		for (int i = 0; i < 10000; i++) {
-			DNA randomStr2 = new DNA(100);
-			DNA kmerrandom = new DNA(100);
-			randomStr2.getIndexHash(kmerrandom);
-		}
-		DNA generateSeq2 = new DNA(10000000);
-		DNA generatekmer2 = new DNA(10000);
-		
-		// getIndexaverage
-		double cnt2 = 0;
-		for (int i = 0; i < 100; i++) {
-			long start = System.nanoTime();
-			generateSeq2.getIndexHash(generatekmer2);
-			long elapse = System.nanoTime() - start;
-			cnt2 += elapse;
-		}
-		double avg2 = cnt2 / 100 * 1e-6;
-		System.out.println("hash: " + avg2);
 	}
 }
