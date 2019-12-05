f7e574125851c58395c6764dabcfd153d0433bb5
GerardProject <gerardklee@hotmail.com>
2019-12-05 10:42:04 -0800

    exception statements added

--- src/main/java/sequence/Sequence.java
+++ src/main/java/sequence/Sequence.java
@@ -6,7 +6,7 @@ public interface Sequence {
 	/**
 	 * Builds the index based on the length of the k-mer.
 	 * @param k length of the k-mer
-	 * @throws Exception 
+	 * @throws Exception throws exception when errors occur on database end
 	 */
 	public void buildIndex(long k) throws Exception;
 	
@@ -15,6 +15,7 @@ public interface Sequence {
 	 * tries to use that index to perform faster look-up.
 	 * @param kmer sub-sequence to search for
 	 * @return list of matching starting positions in this sequence
+	 * @throws Exception throws exception 
 	 */
 	public List<Long> getMatchingPositions(Sequence kmer) throws Exception;
 	
